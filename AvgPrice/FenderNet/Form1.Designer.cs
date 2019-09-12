81b0a14184a5627f414cf4d586b1e68154c04b0c
Dmitri Fomitsjov <dmitri.fomitsjov@tptlive.ee>
2019-09-12 21:12:25 +0300

    avg prices start

---
+++ AvgPrice/FenderNet/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FenderNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.OutputTXT = new System.Windows.Forms.RichTextBox();
+            this.FromTextBox = new System.Windows.Forms.TextBox();
+            this.ToTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FileTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 64);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ValiFail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OutputTXT
+            // 
+            this.OutputTXT.Location = new System.Drawing.Point(617, 15);
+            this.OutputTXT.Margin = new System.Windows.Forms.Padding(4);
+            this.OutputTXT.Name = "OutputTXT";
+            this.OutputTXT.Size = new System.Drawing.Size(323, 174);
+            this.OutputTXT.TabIndex = 2;
+            this.OutputTXT.Text = "";
+            // 
+            // FromTextBox
+            // 
+            this.FromTextBox.Location = new System.Drawing.Point(16, 166);
+            this.FromTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.FromTextBox.Name = "FromTextBox";
+            this.FromTextBox.Size = new System.Drawing.Size(132, 22);
+            this.FromTextBox.TabIndex = 3;
+            // 
+            // ToTextBox
+            // 
+            this.ToTextBox.Location = new System.Drawing.Point(16, 220);
+            this.ToTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ToTextBox.Name = "ToTextBox";
+            this.ToTextBox.Size = new System.Drawing.Size(132, 22);
+            this.ToTextBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 146);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Kust alustada(Min 2)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 201);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Kui palju ridu";
+            // 
+            // FileTextBox
+            // 
+            this.FileTextBox.Enabled = false;
+            this.FileTextBox.Location = new System.Drawing.Point(55, 100);
+            this.FileTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.FileTextBox.Name = "FileTextBox";
+            this.FileTextBox.Size = new System.Drawing.Size(553, 22);
+            this.FileTextBox.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Fail:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 252);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(196, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Otsi keskmist";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(744, 252);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Salvesta";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 303);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.FileTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ToTextBox);
+            this.Controls.Add(this.FromTextBox);
+            this.Controls.Add(this.OutputTXT);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox OutputTXT;
+        private System.Windows.Forms.TextBox FromTextBox;
+        private System.Windows.Forms.TextBox ToTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox FileTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ AvgPrice/FenderNet/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using System.Globalization;
+using OfficeOpenXml;
+
+namespace FenderNet
+{
+    public partial class Form1 : Form
+    {
+
+        String fileName = "";
+        String[] priceResults = new String[10];
+        String[] resultsName = new String[10];
+        String[] excelOutputRow = new String[16];
+        List<String[]> excelRows = new List<String[]>();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /*
+         Look for the intended file        
+        */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+                FileTextBox.Text = fileName;
+            }
+
+
+        }
+        /*
+         Calculate avg prices and save them to new excel. 
+        */
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OutputTXT.Text = "";
+            int from = Convert.ToInt32(FromTextBox.Text);
+            int to = Convert.ToInt32(ToTextBox.Text);
+            float avgPrice = 0;
+            int resultPlace = 0;
+            double priceToCompare = 0;
+            string lookFor = "";
+            int sheet = 1;
+
+            _Application excel = new _Excel.Application();
+            Workbook wb = excel.Workbooks.Open(fileName);
+            Worksheet ws = wb.Worksheets[sheet];
+            int row = from;
+            int count = 0;
+            int delete = 0;
+            
+            while (ws.Cells[row, 1].Value2 != null && delete < to)
+            {
+                // look from source file for prices to look for
+                delete++;
+                lookFor = ws.Cells[row, 5].Value2;
+                priceToCompare = ws.Cells[row, 14].Value2;
+                if (ws.Cells[row, 5].Value2 == null)
+                {
+                    count = 0;
+                    row++;
+                    continue;
+                }
+                // end
+
+
+                string url = "https://www.ebay.com/sch/i.html?_ftrt=901&_sop=12&_sadis=15&_dmd=1&_osacat=0&_ipg=25&_ftrv=1&_from=R40&_trksid=m570.l1313&_nkw=" + lookFor + "&_sacat=0&LH_TitleDesc=1";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader streamreaderEbay = new StreamReader(response.GetResponseStream());
+                String sourceCodeEbay = streamreaderEbay.ReadToEnd();
+                streamreaderEbay.Close();
+                string patternForPrice = "s-item__price";
+                string sentence = sourceCodeEbay;
+
+                // look into the web html and find the prices and save them to array
+                foreach (Match match in Regex.Matches(sentence, patternForPrice))
+                {
+                    string foundOnePrice = sourceCodeEbay.Substring(match.Index + 16, 10);
+                    string price = foundOnePrice.Substring(0, foundOnePrice.IndexOf('<')); // result
+                    count++;
+                    priceResults[resultPlace] = price;
+                    if (count >= 10)
+                    {
+                        break;
+                    }
+
+                    resultPlace++;
+                }
+                // calculate avg price
+                foreach (String result in priceResults)
+                {
+                    avgPrice += float.Parse(result, CultureInfo.InvariantCulture.NumberFormat);
+                }
+                avgPrice = (float)Math.Round(avgPrice / 10); // round up the avg price
+                // create result excel row
+                excelOutputRow = new String[]{ (ws.Cells[row, 1].Value2).ToString("G") ,  lookFor, (priceToCompare).ToString("G"), avgPrice.ToString("G"),
+                    Math.Round((avgPrice - priceToCompare), 2).ToString("G"), priceResults[0], priceResults[1], priceResults[2], priceResults[3], priceResults[4], priceResults[5], priceResults[6],
+                    priceResults[7], priceResults[8], priceResults[9], url};
+                excelRows.Add(excelOutputRow);
+                OutputTXT.Text += "Tehtud " + row + " rida\n";
+                // every ten rows done refresh outup log
+                if (row % 10 == 0) 
+                {
+                    OutputTXT.Text = "";
+                }
+                count = 0;
+                row++;
+                resultPlace = 0;
+                avgPrice = 0;
+
+
+            }
+            wb.Close();
+            // create new excel file for results
+            using (ExcelPackage excelCreate = new ExcelPackage())
+            {
+                excelCreate.Workbook.Worksheets.Add("Tulemused");
+
+                var headerRow = new List<string[]>()
+                    {
+                    new string[] { "ID", "Tootja kood", "Hind", "Keskmine hind", "Hindade vahe",
+                        "Leitud hind 1", "Leitud hind 2", "Leitud hind 3", "Leitud hind 4", "Leitud hind 5", "Leitud hind 6", "Leitud hind 7", "Leitud hind 8", "Leitud hind 9", "Leitud hind 10", "URL" }
+                    };
+
+
+                // Determine the header range (e.g. A1:D1)
+                string headerRange = "A1:" + Char.ConvertFromUtf32(headerRow[0].Length + 64) + "1";
+
+
+                // Target a worksheet
+                var worksheet = excelCreate.Workbook.Worksheets["Tulemused"];
+
+                // Popular header row data
+                worksheet.Cells[headerRange].LoadFromArrays(headerRow);
+                worksheet.Cells[headerRange].AutoFitColumns();
+
+                int rowToWrite = 2;
+                int columnToWrite = 1;
+                foreach (String[] write in excelRows)
+                {
+                    foreach (String rowValue in write)
+                    {
+                        worksheet.Cells[rowToWrite, columnToWrite].Value = rowValue;
+                        columnToWrite++;
+                    }
+                    columnToWrite = 1;
+                    rowToWrite++;
+
+                }
+
+
+                FileInfo excelFile = new FileInfo(fileName.Substring(0, fileName.LastIndexOf(@"\")) + @"\Result.xls");
+                excelCreate.SaveAs(excelFile);
+                System.Diagnostics.Process.Start(fileName.Substring(0, fileName.LastIndexOf(@"\")) + @"\Result.xls");
+            }
+            OutputTXT.Text = "Keskmised hinnad leitud++";
+        }
+        /*
+         Save modified prices to a new file.             
+        */
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String resultFile = (FileTextBox.Text).Substring(0, fileName.LastIndexOf(@"\")) + @"\Result.xls";
+            int sheet = 1;
+            int row = 2;
+            List<string> priceToReplace = new List<string>();
+            List<string> ids = new List<string>();
+            _Excel.Application excel = new _Excel.Application();
+            Workbook wb = excel.Workbooks.Open(resultFile);
+            Worksheet ws = wb.Worksheets[sheet];
+            while (ws.Cells[row, 1].Value2 != null)
+            {
+                priceToReplace.Add("" + ws.Cells[row, 4].Value2);
+                ids.Add(ws.Cells[row, 1].Value2);
+                row++;
+            }
+            
+            wb.Close();
+            wb = excel.Workbooks.Open(fileName);
+            ws = wb.Worksheets[sheet];
+            int replaceCount = ids.Count();
+            row = 2;
+            
+            while(replaceCount > 0)
+            {
+                OutputTXT.Text = "";
+                for (int i = 0; i < ids.Count; i++)
+                {
+                    if (Convert.ToInt32(ids[i]) == Convert.ToInt32(ws.Cells[row, 1].Value2))
+                    {
+                        ws.Cells[row, 14].Value2 = priceToReplace[i];
+                        //ws.Cells[row, 13].Value2 = (Convert.ToDouble(priceToReplace[i]) * 0.8);
+                        ids.RemoveAt(i);
+                        priceToReplace.RemoveAt(i);
+                        OutputTXT.Text += "Teha veel ridu: " + replaceCount;
+                        replaceCount--;
+                        break;
+                    }
+                }
+                row++;
+                
+            }
+            wb.SaveAs((FileTextBox.Text).Substring(0, fileName.LastIndexOf(@"\")) + @"\Replaced.xls");
+            wb.Close();
+            OutputTXT.Text = "Salvestatud";
+            
+        }
+    }
+}
